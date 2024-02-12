d44a6b619c3dd65171f578b082dcc9bf21bfd0ce
Fadi1337 <104858775+Fadi002@users.noreply.github.com>
2024-02-12 14:07:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ __GITHUB__ = "https://github.com/Fadi002"
 
 
 from flask import Flask, jsonify, request, redirect
-import hmac, hashlib, httpx, time, os, random
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad, unpad
+import hmac, hashlib, httpx, time, os, random, binascii, json
+
 def water(text):
     os.system(""); faded = ""
     green = 10
@@ -67,6 +71,46 @@ def parse_request_data(request_data):
         key, value = pair.split('=')
         data_dict[key] = value
     return data_dict
+
+class encryption:
+    '''
+    CREDITS TO KEYAUTH
+    '''
+    @staticmethod
+    def encrypt_string(plain_text, key, iv):
+        plain_text = pad(plain_text, 16)
+        aes_instance = AES.new(key, AES.MODE_CBC, iv)
+        raw_out = aes_instance.encrypt(plain_text)
+        return binascii.hexlify(raw_out)
+
+    @staticmethod
+    def decrypt_string(cipher_text, key, iv):
+        cipher_text = binascii.unhexlify(cipher_text)
+        aes_instance = AES.new(key, AES.MODE_CBC, iv)
+        cipher_text = aes_instance.decrypt(cipher_text)
+        return unpad(cipher_text, 16)
+
+    @staticmethod
+    def encrypt(message, enc_key, iv):
+        try:
+            _key = SHA256.new(enc_key.encode()).hexdigest()[:32]
+            _iv = SHA256.new(iv.encode()).hexdigest()[:16]
+            return encryption.encrypt_string(message.encode(), _key.encode(), _iv.encode()).decode()
+        except:
+            print("Encryption error. Make sure your app details are correct, see response below")
+            print("Response: " + message)
+            time.sleep(3)
+
+    @staticmethod
+    def decrypt(message, enc_key, iv):
+        try:
+            _key = SHA256.new(enc_key.encode()).hexdigest()[:32]
+            _iv = SHA256.new(iv.encode()).hexdigest()[:16]
+            return encryption.decrypt_string(message.encode(), _key.encode(), _iv.encode()).decode()
+        except:
+            print("Encryption error. Make sure your app details are correct, see response below")
+            print("Response: " + message)
+            time.sleep(3)
 
 @app.route('/', methods=['GET'])
 def home():return HOME_PAGE
